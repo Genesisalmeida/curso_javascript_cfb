20d0786bd569115095ea882f154b5937a72346f7
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-10-15 04:43:40 -0300

    commit

---
+++ for-in-for-of/for-in-for-of.js
@@ -0,0 +1,11 @@
+//for in //
+const obj =['carro', 'casa', 'bola']
+for(let i in obj){
+    console.log(obj[i])
+}
+
+//for of //
+
+for(let i of obj){
+    console.log(i) // aqui usamos so indice 'i' para imprimer o loop//
+}
