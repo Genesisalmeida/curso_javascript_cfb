831464643a38c8b49d2f1dedb7eb962b60b71fce
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-10-18 05:10:00 -0300

    commit

--- break-continue/break.js
+++ break-continue/break.js
@@ -1,26 +1,25 @@
 // break e continue em java script//
  
-
 let n = 0 ; 
 let cont = 100;
 while(n <= cont){
     console.log(n)
-    if(n>=20){      // aui um exemplo do break sendo execultado
+    if(n>=20){      // aqui um exemplo do break sendo execultado
         break
         
     }
   n++
 }
 
-
 //continue
 
 for(let i = 0; i<=10; i++){
     if(i%2 == 0){
         continue; // ele para a execução do mod 2 e retorna os numeros impares
+            
         }
-     console.log('numeros pares: '+ i )
+        console.log(i)
+     
         
-
-    
 }
+
