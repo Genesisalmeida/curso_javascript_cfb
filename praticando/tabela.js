f619157c60d70cb7e5b3ecbda0e52294b76ba40e
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-10-25 04:58:34 -0300

    commit

--- praticando/tabela.js
+++ praticando/tabela.js
@@ -1,11 +1,20 @@
 
 let n1 = document.getElementsByClassName('nota');
-let n2 = document.getElementsByClassName('medianum')
-let media = 0;
-    for(let i in n1){
-        let n = parseFloat(n1[i].innerText);
-        if(!isNaN(n)){
-           media += n ; 
-        }
-     }
+let n2 = document.getElementsByClassName('medianum')[0];// acessando o item do html(colection sem loop)
+let input = document.querySelector('input');
+let input1 = document.querySelector('.valor')
+let valordigitado;
+let valor1;
+let valor2;
 
+ valordigitado = valor1+ valor2
+let button = document.addEventListener('click',  calc=()=>{
+  valor1=parseFloat( input.value );
+  valor2 =parseFloat(input1.value);
+  valordigitado = valor1 + valor2
+  let media=valordigitado / 2
+
+ n2.innerHTML = media.toFixed(1)
+})
+
+let media = 0;
