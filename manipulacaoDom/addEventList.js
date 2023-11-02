bd6933844e74bf51bf08a7525b07b28679545416
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-11-02 04:45:54 -0300

    praticando

---
+++ manipulacaoDom/addEventList.js
@@ -0,0 +1,16 @@
+//add eventeList//
+const div = document.querySelectorAll('div');
+let l =[...div];
+
+l.map((e)=>{
+   e.addEventListener('click',(l)=>{ // como usar o addEventList dentro do 'Map'...
+        console.log(`${e.tagName} foi cricado`)
+        e.innerHTML='Modificou'
+   }) 
+})
+div.addEventListener('click', (e)=>{// evento de click.. 
+    console.log(e.target) // auqi estamos capturando o event
+    div.innerHTML='Amém';
+    div.style.color='blue';
+})
+
