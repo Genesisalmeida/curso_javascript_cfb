6684f196f9e8ac4089391ca1d380990cef71fde5
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-11-09 10:17:43 -0300

    creatElement

---
+++ manipulacaoDom/CriandoElement/criadoElementos.js
@@ -0,0 +1,23 @@
+const div =document.querySelector('.box');
+const divs = [...document.querySelectorAll('.obj')]
+ 
+const cursos = ['php','css','javascript','html'];
+
+
+cursos.map((el)=>{
+   const newElement = document.createElement('h1');
+    newElement.setAttribute('class', 'obj'); // 
+    div.appendChild(newElement)
+    newElement.innerHTML=el
+});
+
+
+divs.map((e)=>{
+const newElement =document.createElement('h1');// criando novo elemento
+newElement.innerHTML='jesus é  bom';// adicionado valor ao elemento
+newElement.setAttribute('class','color')
+e.appendChild(newElement) // adicionando novo elemento criado a div
+})
+
+
+
