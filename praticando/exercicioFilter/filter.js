49bfebff3caf69825c98105752641cd7e3fcfe32
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-11-15 10:51:45 -0300

    aula_remove

--- praticando/exercicioFilter/filter.js
+++ praticando/exercicioFilter/filter.js
@@ -1,14 +1,32 @@
 const buttom = document.querySelector('#selection');
+const buttomRemove = document.querySelector('#remove');
 
-buttom.addEventListener('click',(evt)=>{
+const radioSelecionado=()=>{ // fuction para selecionar os elementos
     const todosradio = [...document.querySelectorAll('input[type=radio]')];
-    let radiosSelecionados= todosradio.filter((elem)=>{
-        return elem.checked
-    })
-    radiosSelecionados=radiosSelecionados[0]; //aqui estamos pegamos somente o conteudo do radio selecionado na posição 0
-        const input = document.querySelector('#input')
-        const cursosSelecionados = radiosSelecionados.parentNode.firstChild.textContent
-        input.value = cursosSelecionados
-  // input.value =  radiosSelecionados.value
-
+    const rs= todosradio.filter((elem)=>{
+        return elem.checked;
+    });
+    return rs[0];//aqui estamos pegamos somente o conteudo do radio selecionado na posição 0
+}
+buttom.addEventListener('click',(evt)=>{
+        const input = document.querySelector('#input');
+        const radiosSelecionados= radioSelecionado();
+        if(radiosSelecionados!=undefined){
+            const cursosSelecionados= radiosSelecionados.parentNode.firstChild.textContent
+            input.value = cursosSelecionados;
+        }else{
+            console.log('selecione um curso')
+        }
+    });
+buttomRemove.addEventListener('click',(evt)=>{
+    const elementselect = radioSelecionado()//aqui ja estamos usando a funçao'radioSelecionado no buttomremove//
+    if(elementselect!=undefined){ // aqui estamos tratando o erro do consoole undefined
+        const cursosSelecionados = elementselect.parentNode
+        cursosSelecionados.remove()
+        const input = document.querySelector('#input');
+        input.value=''
+    }else{
+        console.log('selecione um curso')
+    }
+  
 })
