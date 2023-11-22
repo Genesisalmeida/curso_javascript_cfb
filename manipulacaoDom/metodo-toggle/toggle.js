a298920c803b216a484c369eca5df925cf996638
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2023-11-22 10:13:38 -0300

    fim_projeto

--- manipulacaoDom/metodo-toggle/toggle.js
+++ manipulacaoDom/metodo-toggle/toggle.js
@@ -1,5 +1,11 @@
+const caixa = document.querySelector('.caixa');
+const addAntes = document.querySelector('#ada');
+const addDepois = document.querySelector('#add');
+const input = document.querySelector('#input');
 const selection = document.querySelector('#selection');
+const remove = document.querySelector('#remove');
 const cursos = [...document.querySelectorAll('.liguagem')];
+
 const selecionados = ()=>{
     cursos.map((ele)=>{
         ele.addEventListener('click',(evt)=>{
@@ -8,32 +14,64 @@ const selecionados = ()=>{
         })
     })
 };
+
 const tirarSelecao=()=>{
     cursos.map((el)=>{
         el.classList.remove('selecionado');
     })
 };
-selecionados()
+
+selecionados();
 
 const cursosSelection=()=>{
-    const rs= cursos.filter((elem,i)=>{
-      return elem.classList.contains('selecionado' );
-  
-    
-})
-    return rs[0]
-}
+    const selecionado =[...document.querySelectorAll('.selecionado')];
+     return selecionado[0]
+};
 
-selection.addEventListener('click',()=>{
-    const cursoSelecionado = cursosSelection().textContent;
-    try{
-      
-  alert(`Curso selecionado foi ${cursoSelecionado}`)
-              
-    }
-    catch{
-        alert('Selecione um curso')
-    }  
-    
+const criarNovocurso=()=>{ //função  que cria elemento//
    
-})
+    if (input.value !== '') {
+        const element = document.createElement('div');
+        element.innerHTML = input.value;
+        element.setAttribute('class', 'liguagem');
+        caixa.append(element);
+        return element;
+    } else {
+           alert('Digite o nome do curso');
+                return null;
+    }  
+}
+addAntes.addEventListener('click',()=>{
+    const addcursos = criarNovocurso();
+    const selection = cursosSelection();
+    if(addcursos && selection){
+        caixa.insertBefore(addcursos,selection)
+    }
+});
+
+addDepois.addEventListener('click',()=>{
+    const addcursos = criarNovocurso();
+    const selection = cursosSelection();
+    if(addcursos && selection){
+        caixa.insertBefore(addcursos,selection.nextSibling)
+    }
+});
+
+selection.addEventListener('click',(evt)=>{
+    const cursoSelecionado = cursosSelection();
+        if(cursoSelecionado!==undefined){
+            alert(`Curso Selecionado foi ${cursoSelecionado.innerHTML}`)
+        }else{
+            alert('Selecione um curso');
+        }
+});
+
+remove.addEventListener('click',()=>{
+    const cursoSelecionado = cursosSelection()
+        if(cursoSelecionado!==undefined){
+        cursoSelecionado.remove() 
+        }else{
+            alert('Selecione um curso para remover');
+        }
+});
+
