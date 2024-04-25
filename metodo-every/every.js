4b29d30e4ade7c4b184398f418039200f93dffe3
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2024-04-25 11:21:41 -0300

    //aul_some

--- metodo-every/every.js
+++ metodo-every/every.js
@@ -13,14 +13,15 @@ const box_result = document.querySelector("#resultado")
 const elemento = [33,6,41,23,14];
 divArray.innerHTML=` [${elemento}]`;
 button.addEventListener('click',(evt)=>{
+    const array = ''
     const ret =  elemento.every((n,i)=>{
         if(n<18){
-            box_result.innerHTML='Não conforme na posição' +i
+            box_result.innerHTML='Não conforme na posição' + i
          }
-         return n>=18
+          return n>=18 
    
     })
-    if(ret){
+    if(ret){//Então, após a execução do método every, a variável ret conterá true se todos os elementos do array forem maiores ou iguais a 18, e false se pelo menos um elemento for menor que 18. Em seguida, o if (ret) verifica o valor de ret. Se for true, isso significa que todos os elementos são maiores ou iguais a 18, então exibimos a mensagem "Array ok" na box_result. Se for false, isso significa que pelo menos um elemento é menor que 18, então a mensagem correspondente já foi exibida dentro da função de callback do every, e não precisamos fazer mais nada.
         box_result.innerHTML='Array ok'
     }
 })
---
+++ metodo_some/some.js
@@ -0,0 +1,25 @@
+//metodo some//
+// O método some() em JavaScript é uma maneira de verificar se pelo menos um dos elementos em um array satisfaz uma determinada condição. Ele retorna true se pelo menos um elemento passar no teste especificado pela função de callback, caso contrário, retorna false.
+
+// Em resumo, o some() é como uma pergunta: "Existe pelo menos um elemento que atenda a essa condição?" Se a resposta for sim para pelo menos um elemento, o some() retorna true; se não, retorna false. É útil quando você precisa saber se pelo menos uma condição é verdadeira em um array.
+
+
+const divArray = document.querySelector("#array");
+const button = document.querySelector("#btnPesquisar");
+const box_result = document.querySelector("#resultado")
+const elemento = [2,2,5,16,19,11];
+divArray.innerHTML=` [${elemento}]`;
+
+button.addEventListener('click',()=>{
+    const ret = elemento.some((elm, posi)=>{
+        if(elm<18){
+              box_result.innerHTML='menor idade na posição:' + posi;
+        }
+        return  elm >=18;
+
+    })
+    if(ret){
+        box_result.innerHTML='maior de idade';
+    }
+});
+
