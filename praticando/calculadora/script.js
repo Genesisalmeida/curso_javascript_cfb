4c99dc2b01e6b31ab108c61965124e3c7fa2b5cb
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2024-06-04 20:54:58 -0300

    add_cal

--- praticando/calculadora/script.js
+++ praticando/calculadora/script.js
@@ -1,1 +1,25 @@
-console.log(hgg)
+const teclasNum = [...document.querySelectorAll('.num')];
+const teclasOp = [...document.querySelectorAll('.op')];
+const teclaRes = document.querySelector('.res');
+const display = document.querySelector('.display');
+const tOn = document.querySelector('#tOn');
+const clear = document.querySelector('#limpar');
+
+//teclas numericas
+teclasNum.map((elemen)=>{
+  elemen.addEventListener('click',(evt)=>{
+   display.innerHTML+=evt.target.innerHTML
+  });
+});
+
+//teclas operações//
+teclasOp.map((elemen)=>{
+    elemen.addEventListener('click',(evt)=>{
+     display.innerHTML+=evt.target.innerHTML
+    });
+  });
+
+ //zerando o display// 
+clear.addEventListener('click',()=>{
+      display.innerHTML=""
+})  
