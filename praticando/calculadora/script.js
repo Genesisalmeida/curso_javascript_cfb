21b295fb9b471c553bee9cd56d655062cf2df088
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2024-06-10 11:34:37 -0300

    add

--- praticando/calculadora/script.js
+++ praticando/calculadora/script.js
@@ -1,35 +1,38 @@
 const teclasNum = [...document.querySelectorAll('.num')];
 const teclasOp = [...document.querySelectorAll('.op')];
 const teclaRes = document.querySelector('.res');
+const calculadora = document.querySelector('.calc')
 const display = document.querySelector('.display');
-const tOn = document.querySelector('#tOn');
+const tcopy = document.querySelector('#tcopy');
 const clear = document.querySelector('#limpar');
 const teclaIgual = document.querySelector('#tigual');
-console.log(teclaIgual)
+const teste = document.querySelector('#teste');
+const gavetaon = document.querySelector('.gavetaon')
+
 
 let sinal = false;
-let decimal = false
+let decimal = false;
 
 //teclas numericas
 teclasNum.map((elemen)=>{
   elemen.addEventListener('click',(evt)=>{
-     sinal=false
+     sinal=false;
      if(evt.target.innerHTML==','){
         if(!decimal){
-          decimal=true
+          decimal=true;
           if(display.innerHTML=='0'){
-              display.innerHTML='0,'
+              display.innerHTML='0,';
           }else{
-            display.innerHTML+=evt.target.innerHTML
-          }
+            display.innerHTML+=evt.target.innerHTML;
+          };
           
-      }
+      };
     }else{
         if(display.innerHTML=='0'){
-            display.innerHTML=''
-        }
-        display.innerHTML+=evt.target.innerHTML
-    }  
+            display.innerHTML='';
+        };
+        display.innerHTML+=evt.target.innerHTML;
+    };
 
   });
 });
@@ -40,49 +43,57 @@ teclasOp.map((elemen)=>{
         sinal=true
         if(display.innerHTML=='0'){
            display.innerHTML=''
-        }
+        };
         if(evt.target.innerHTML=='x'){
             display.innerHTML+='*'
         }else{
             display.innerHTML+=evt.target.innerHTML
-        }
+        };
         
-      }
+      };
       
     });
   });
  //zerando o display// 
 clear.addEventListener('click',()=>{
-      sinal=false
-      decimal = false
-      display.innerHTML=""
-})  
+      sinal=false;
+      decimal = false;
+      display.innerHTML="";
+})  ;
 
 //tecla operação//
 
 teclaIgual.addEventListener('click',()=>{
-  sinal=false
-  decimal = false
+  sinal=false;
+  decimal = false;
     const res = eval(display.innerHTML)
       if(res!=undefined){
-          display.innerHTML = res
+          display.innerHTML = res   // eval A função Eval avalia o expressão de cadeia de caracteres e retorna seu valor. Por exemplo, Eval("1 + 1") retorna 2. Se você passar para a função Eval uma cadeia de caracteres que contém o nome de uma função, a função Eval retornará o valor retornado da função.
       }else{
         alert('digite valores válidos')
-      } 
+      } ;
+});
+
+//copiar para area de transferência//
+
+tcopy.addEventListener('click',(evt)=>{
+    //navigator.clipboard.writeText(display.innerHTML);~
+    teste.select(); // Bom usar o select antes ,principalmente em dispositivos mobile
+    navigator.clipboard.writeText(teste.value)
+});
+
+
+
+gavetaon.addEventListener('click',(evt)=>{
+  const delay = 500
+  setTimeout(()=>{
+       gavetaon.classList.toggle('gavetaof') 
+    if(calculadora.style.display =='flex')
+      calculadora.style.display='none'
+    else{
+      calculadora.style.display= 'flex'
+    }
+  },delay)
+
     
-    
-   // eval A função Eval avalia o expressão de cadeia de caracteres e retorna seu valor. Por exemplo, Eval("1 + 1") retorna 2. Se você passar para a função Eval uma cadeia de caracteres que contém o nome de uma função, a função Eval retornará o valor retornado da função.
-  
-})  
-
-
-
-
-
-
-
-// botton of,on//
-// tOn.addEventListener('click',()=>{
-//   display.classList.toggle('teste')
-//   display.innerHTML="0"
-// })
+})
