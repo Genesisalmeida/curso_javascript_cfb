510d1541ed808a6a5201ba92135c3d79fba32b57
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2024-07-12 18:52:36 -0300

    date_hora

---
+++ data_hora_js/dataHora.js
@@ -0,0 +1,40 @@
+// data e hora
+
+const date = document.querySelector('.date')
+let data = new Date()
+
+
+const data_r = data.getDate()+'/'+ data.getMonth()+'/'+data.getFullYear()
+
+date.innerHTML=data_r
+date.style.color='red'
+
+// Métodos para obtenção de informações
+// getDate(): Retorna o dia do mês (1-31).
+// getDay(): Retorna o dia da semana (0-6, onde 0 é domingo e 6 é sábado).
+// getMonth(): Retorna o mês (0-11, onde 0 é janeiro e 11 é dezembro).
+// getFullYear(): Retorna o ano completo.
+// getHours(): Retorna a hora (0-23).
+// getMinutes(): Retorna os minutos (0-59).
+// getSeconds(): Retorna os segundos (0-59).
+// getMilliseconds(): Retorna os milissegundos (0-999).
+// getTime(): Retorna o valor numérico da data em milissegundos desde 1 de janeiro de 1970 00:00:00 UTC.
+
+// Métodos para definir informações//
+
+// setDate(dia): Define o dia do mês.
+// setMonth(mes): Define o mês (0-11).
+// setFullYear(ano): Define o ano.
+// setHours(horas): Define a hora (0-23).
+// setMinutes(minutos): Define os minutos (0-59).
+// setSeconds(segundos): Define os segundos (0-59).
+// setMilliseconds(milissegundos): Define os milissegundos (0-999).
+// setTime(tempoMillis): Define a data com base no valor numérico em milissegundos desde 1 de janeiro de 1970 00:00:00 UTC.
+
+// Outros métodos úteis
+
+// toDateString(): Retorna a parte da data em formato legível (sem a hora).
+// toTimeString(): Retorna a parte da hora em formato legível (sem a data).
+// toISOString(): Retorna a data no formato ISO 8601.
+
+
--- promise/promise.js
+++ promise/promise.js
@@ -26,11 +26,13 @@ const butyes = document.querySelector('.butYes');
 const numero = document.querySelector('.numero')
 butyes.addEventListener('click',()=>{
     numero.innerHTML='Processando....'
+ 
     minhaPromese()
         .then((ok)=>{
             numero.innerHTML=ok
             numero.classList.add('ok')
             numero.classList.remove('erro')
+            console.log(re)
         }).catch((erro)=>{
             numero.innerHTML=erro
             numero.classList.remove('ok')
@@ -51,5 +53,5 @@ const minhaPromese = ()=>{
     })
     return promise
 }
-
+console.log(minhaPromese())
 numero.innerHTML='Esperando....'
