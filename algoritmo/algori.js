13fd2f6ace220f8b5b75158d017543a4552c3edd
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2024-10-23 11:11:02 -0300

    dados

--- algoritmo/algori.js
+++ algoritmo/algori.js
@@ -195,16 +195,143 @@
 // parImpar(v);
 
 // fibonacci
-let fib = [0,1];
+// let fib = [0,1];
 
-function fibonacci(n,val){
-   for(let i = 2; i < val ; i++){
-      n[i] = n[i-1] + n[i-2]
+// function fibonacci(n,val){
+//    for(let i = 2; i < val ; i++){
+// //       n[i] = n[i-1] + n[i-2]
      
-   }
-   return n
+// //    }
+// //    return n
    
+// // }
+// // console.log(fibonacci(fib,100))
+
+// function parImáp(n){
+// //
+//     return n%2 == 0? ` O numero ${n} é par!`:`Numero ${n} é impar!` 
+// //  
+// }
+// function fibonacci(){
+    
+//     const fib = [0,1];
+//     for(let i = 2; i<10; i++){
+//         fib[i] = fib[i-1] + fib[i-2]
+        
+//     }
+//     return fib[fib.length - 1]
+    
+// }
+// console.log(parImáp(fibonacci()));
+
+// function fatori (n){
+//     let fatorial = 1 ;
+//     for(let i = n ; i>1; i--){
+//         fatorial*= i
+//     }
+//     return fatorial
+
+// }
+// console.log(fatori(100))
+// let vetor = [15,54,7,8,9,10]
+
+// for(let i = 0; i<vetor.length; i++){
+//     if(vetor[i]%2==0){
+//         console.log(`O numero ${vetor[i]} é par e eta na posiçaõ ${i}`)
+//     }else{
+//         console.log(`O numero ${vetor[i]} é impar esta na posiçaõ ${i}`)
+//     }
+// }
+
+// console.log(vetor)
+
+// //listagem de alunos
+// let nomeAlunos = [];
+// let idadeAlunos = [];
+// let continuar = prompt('Bom dia! Quer continuar? "sim" ou "não" ');
+
+// while (continuar === 'sim') {
+//     let nome = prompt('Digite o nome de um aluno:');
+//     let idade = Number(prompt('Digite a idade do aluno:'));
+
+//     nomeAlunos.push(nome);
+//     idadeAlunos.push(idade);
+
+//     continuar = prompt('Quer continuar? "sim" ou "não" ');
+// }
+
+// console.log('Nomes dos alunos:', nomeAlunos);
+// console.log('Idades dos alunos:', idadeAlunos);
+
+//
+
+// let nome = []
+// let somar = 0
+// for(let i = 0; i<4; i++){
+//     let v = prompt('Digites nomes ')
+//     nome.push(v)
+// }
+
+//     let  letra = 'c'
+//  const bucar = nome.filter((ele)=>{
+//    return ele.startsWith(letra)
+    
+//  })
+
+//     console.log(bucar)
+
+const v = [4, 2, 8, 9, 7, 6, 3, -1];
+
+function bubbleSort(arr){
+    for(let i =0; i<arr.length; i++){
+        for(let j = 0 ; j<arr.length; j++){
+            if(arr[j]>arr[j + 1]){                                  
+              let aux= arr[j]
+              arr[j] = arr[j + 1]
+              arr[j + 1] =aux
+            }
+        }
+    }
+    return arr
 }
-console.log(fibonacci(fib,100))
 
 
+console.log(bubbleSort(v));  // Exibe o array ordenado
+
+function bubooSoft(arr){
+   for(let i = 0; i<arr.length ; i++){
+        for(let j = 0; j<arr.length; j++){
+            if(arr[j]>arr[j+1]){
+               let aux = 0
+               aux = arr[j]
+               arr[j] = arr[j+1]
+               arr[j+1]=aux     
+            }
+        }
+   }
+
+  }
+ console.log(bubbleSort(v))
+
+ const fib = [0,1]
+
+ for(let i = 2; i<5; i++){
+    fib[i] = fib[i -1] + fib[i-2]
+
+ }
+ console.log(fib)
+
+let d = [9,8,7,6,5,4,3,2,1]
+
+for(let i = 0; i<d.length; i++){
+        for(let j = 0 ; j<d.length; j++){
+            if(d[j]> d[j+1]){
+                let tem = 0
+                tem = d[j]
+                d[j] = d[j+1]
+                d[j+1] = tem
+            }
+        }
+}
+
+console.log(d)
--- estruturas_dados/dados.js
+++ estruturas_dados/dados.js
@@ -1,47 +1,161 @@
 // PILHAS//
 
-class Stack {
-    constructor() {
-        this.itens = [] // Inicializa um array vazio para armazenar os elementos da pilha
-    }
+// class Stack {
+//     constructor() {
+//         this.itens = [] // Inicializa um array vazio para armazenar os elementos da pilha
+//     }
 
-    push(element) {
-        this.itens.push(element) // Adiciona um elemento ao topo da pilha (fim do array)
-    }
+//     push(element) {
+//         this.itens.push(element) // Adiciona um elemento ao topo da pilha (fim do array)
+//     }
 
-    pop() {
-        this.itens.pop() // Remove e retorna o elemento do topo da pilha (último do array)
-    }
+//     pop() {
+//         this.itens.pop() // Remove e retorna o elemento do topo da pilha (último do array)
+//     }
 
-    peek() {
-        return this.itens[this.itens.length - 1] // Retorna o elemento do topo da pilha sem removê-lo
-    }
+// //     peek() {
+// //         return this.itens[this.itens.length - 1] // Retorna o elemento do topo da pilha sem removê-lo
+// //     }
 
-    isEmpty() {
-        return this.itens.length === 0 ? 'A pilha esta vazio' : 'A pilha não está vazia' 
-        // Verifica se a pilha está vazia. Se o comprimento do array for 0, retorna 'vazio'. Caso contrário, 'não está vazia'
-    }
-    clear() {
-        this.itens = [] // Limpa a pilha, removendo todos os elementos (tornando o array vazio)
-    }
-    size(){
-     return   this.itens.length
-    }
-}    
-const e = new Stack()
-e.push(10)
-e.push(5)
-e.push(4)
-e.push(10)
-e.push(8)
-e.pop()
-e.pop()
-console.log(e.isEmpty())
-console.log(e.size())
-console.log(e.peek()) // exibe 8
+// //     isEmpty() {
+// //         return this.itens.length === 0 ? 'A pilha esta vazio' : 'A pilha não está vazia' 
+// //         // Verifica se a pilha está vazia. Se o comprimento do array for 0, retorna 'vazio'. Caso contrário, 'não está vazia'
+// //     }
+// //     clear() {
+// //         this.itens = [] // Limpa a pilha, removendo todos os elementos (tornando o array vazio)
+// //     }
+// //     size(){
+// //      return   this.itens.length
+// //     }
+// // }    
+// // const e = new Stack()
+// // e.push(10)
+// // e.push(5)
+// // e.push(4)
+// // e.push(10)
+// // e.push(8)
+// // e.pop()
+// // console.log(e.isEmpty())
+// // console.log(e.size())
+// // console.log(e.peek()) // exibe 8
 
-//e.clear()
+// // //e.clear()
+// // console.log(e.itens)
+
+// //Uma classe js baseada em objeto
+// class Stack {
+//     #itens; // Propriedade privada para armazenar os itens da pilha
+//     #cont;  // Propriedade privada para controlar a contagem de elementos
+
+//     constructor() {
+//         this.#cont = 0; // Inicializa a contagem de elementos
+//         this.#itens = {}; // Inicializa os itens como um objeto vazio
+//     }
+
+//     push(element) {
+//         this.#itens[this.#cont] = element; // Adiciona o elemento no topo da pilha
+//         this.#cont++; // Incrementa a contagem
+//     }
+
+//     pop() {
+//         if (this.isEmpty()) {
+//             return "A pilha está vazia"; // Retorna mensagem se a pilha estiver vazia
+//         }
+//         this.#cont--; // Decrementa a contagem
+//         const removed = this.#itens[this.#cont]; // Obtém o elemento removido
+//         delete this.#itens[this.#cont]; // Remove o elemento do objeto
+//         return removed; // Retorna o elemento removido
+//     }
+
+//     size() {
+//         return this.#cont; // Retorna o número de elementos na pilha
+//     }
+
+//     isEmpty() {
+//         return this.#cont === 0; // Verifica se a pilha está vazia
+//     }
+
+//     peek() {
+//         if (this.isEmpty()) {
+//             return "A pilha está vazia"; // Retorna mensagem se a pilha estiver vazia
+//         }
+//         return this.#itens[this.#cont - 1]; // Retorna o elemento no topo sem removê-lo
+//     }
+
+//     clear() {
+//         this.#itens = {}; // Esvazia os itens
+//         this.#cont = 0; // Reinicia a contagem
+//     }
+// }
+
+// // Exemplo de uso
+// let pilha = new Stack();
+// pilha.push(10);
+// pilha.push(20);
+// console.log(pilha.pop());  // 20
+// console.log(pilha.size()); // 1
+// console.log(pilha.peek()); // 10
+// pilha.clear();             // Esvazia a pilha
+// console.log(pilha.isEmpty()); // true
 
 
-console.log(e.itens)
+// Resolvendo  problemas usando pilha
+class Pilha {
+    constructor(){
+        this.item = []
+    }
+    push(ele){
+        this.item.push(ele)
+    }
+    isEmpty(){
+        return this.item.length === 0
+    }
+    pop(){
+        return this.item.pop();
+    }
+}
+// function decimaBinario(n){
+//     const remPilha = new Pilha();
+//     let number = n;
+//     let rem;
+//     let binaryString ='';
+//     while(number > 0){
+//         rem = Math.floor(number % 2)
+//         remPilha.push(rem)
+//         number = Math.floor(number / 2)
+//         console.log(rem)
+//     }
+//     while(!remPilha. isEmpty()){
+//      binaryString+=remPilha.pop().toString()
+
+//     }
+//     return binaryString
+   
+   
+// }
+
+// console.log(decimaBinario(4));
+
+//Conversor de base
+
+function baseConversor(decimal , base){
+    const remPilha = new Pilha();
+    const digits = '0123456789ABCDEFGHIJKMNOPQRSTUVWXYZ';
+    let number = decimal;
+    let rem;
+    let baseSring = '';
+    if(!(base >= 2 && base<= 36)){
+        return '';
+    }
+    while(number > 0){
+        rem = Math.floor(number % base);
+        remPilha.push(rem);
+        number = Math.floor(number/ base)
+    }
+    while(!remPilha.isEmpty()){
+        baseSring+= digits[remPilha.pop()]
+    }
+    return baseSring
+}
+console.log(baseConversor(100345, 15))
 
