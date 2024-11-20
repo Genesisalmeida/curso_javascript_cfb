6d7444f9ef93f2e0d85a8962328d1fcb34ae335d
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2024-11-20 09:50:07 -0300

    export

--- js_export/export.js
+++ js_export/export.js
@@ -1,4 +1,21 @@
-const cores = ['verde','amarelo','rosa','azul']
-const car = ['gol','palio','polo','fiesta']
+//const cores = ['verde','amarelo','rosa','azul']
+// const car = ['gol','palio','polo','fiesta']
 
-export{car,cores}
+// // const getCurso=()=>{  // podemos importar function
+// //     return cores 
+// // }
+
+// export{car,cores} // Aqui estamos  exportado 
+// // export default getCurso // usamos default para definir como padrão,so podemos ter uma esportação com  default
+
+// export default function getCores(){ // podemos fazer exportação com default diretamente na function
+//     return cores
+// }
+
+
+
+//IMPORTAÇÕES NOMEADAS
+const cores = ['verde','amarelo','rosa','azul'];
+const car ='palio'
+
+export {cores,car}
--- modulos/modulos.js
+++ modulos/modulos.js
@@ -1,4 +1,18 @@
-import { car,cores } from "../js_export/export.js";
+// import getCurso,{ car,cores} from "../js_export/export.js";
 
-console.log(car)
-console.log(cores)
+// console.log(car)
+// console.log(cores)
+// console.log(getCurso())
+
+
+
+//IMPORTAÇÕES NOMEADAS
+
+
+// import {cores as c,car as B}from"../js_export/export.js"  // aqui estamos renomeando cores para 'c' usando 'as' ,agora podemos ultilizar cores chamando c.
+// console.log(c)// usando cores ja renomeada com 'c';
+import* as m_cursos from"../js_export/export.js"// Uma forma de exporta tudo  de um so vez, usando' *' e o 'as ' seguido do nome que vc escolher para nomear o import, ex: m_cursos 
+
+console.log(m_cursos.car) // usando os arquivos exportado,colocamos m_cursos seguido de '.' ponto  para escolhermos o que queremos usar;
+
+console.log(m_cursos.cores)
