0d7a623de13378d8371b5b541fddc3b86b87a976
Genesisalmeida <142318459+Genesisalmeida@users.noreply.github.com>
2025-03-13 08:16:15 -0300

    .

--- animacao_js/animacao.js
+++ animacao_js/animacao.js
@@ -1,21 +1,58 @@
-const car = document.querySelector('.carro')
-const btn_di = document.querySelector('#bt_dire')
-const btn_esq = document.querySelector('#bt_esq')   
-let cont = 1
+// const car = document.querySelector('.carro');
+// const stop = document.querySelector('#stop');  
+// const btn_di = document.querySelector('#bt_dire');
+// const btn_esq = document.querySelector('#bt_esq');   
+// const anima = null
+// const init= ()=>{
+//     car.style=`position:relative;left:0px`;
+   
+// }
+//   btn_di.addEventListener('click',()=>{
+//     setInterval(()=>{
+
+//    }) 
+//     let pos =parseInt(car.style.left);
+//     pos+=10;
+//     car.style.left = `${pos}px`;
+
+//     })  
+//     btn_esq.addEventListener('click',()=>{
+//         car.style.left = parseInt(car.style.left)-10 +"px";
+//     })
+
+//  window.onload=init;
+
+ 
+
+//animação automatica 
+
+const car = document.querySelector('.carro');
+const stope = document.querySelector('#stop');  
+const btn_di = document.querySelector('#bt_dire');
+const btn_esq = document.querySelector('#bt_esq');   
+let anima = null
 const init= ()=>{
     car.style=`position:relative;left:0px`;
-   
 }
-  btn_di.addEventListener('click',()=>{
 
-    let pos =parseInt(car.style.left)
-    pos+=10
-    car.style=`position:relative;left:${pos}px`
+const mover=(dir)=>{
+  car.style.left = parseInt(car.style.left) + (10*dir) +"px";
+}
 
-        
+btn_di.addEventListener('click',()=>{
+    clearInterval(anima)
+       anima = setInterval(mover,200,1)
+
     })  
-    btn_esq.addEventListener('click',()=>{
-        car.style.left = parseInt(car.style.left)-10 +"px"
+
+btn_esq.addEventListener('click',()=>{
+      clearInterval(anima)
+       anima = setInterval(mover,200,-1)
     })
 
- window.onload=init
+stope.addEventListener('click',()=>{
+      clearInterval(anima) //limpar intervalo
+   
+   })
+
+ window.onload=init;
